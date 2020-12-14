9aae8ac230d4c23cb95ea19db357a4b1b108612b
caneregin <36435160+caneregin@users.noreply.github.com>
2020-12-15 00:51:48 +0300

    list DB den mysql DB geçiliyor

--- src/main/java/com/example/ecommerce/api/PersonController.java
+++ src/main/java/com/example/ecommerce/api/PersonController.java
@@ -3,10 +3,15 @@ package com.example.ecommerce.api;
 import java.util.List;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +27,7 @@ public class PersonController {
 		this.personService = personService;
 	}
 	@PostMapping
-	public void addPerson(@RequestBody Person person) {
+	public void addPerson(@Valid @NonNull @RequestBody Person person) {
 		personService.addPerson(person);
 	}
 	@GetMapping
@@ -32,5 +37,14 @@ public class PersonController {
 	@GetMapping(path = "{id}")
 	public Person getPersonById(@PathVariable("id") UUID id) {
 		return personService.getPersonById(id).orElse(null);
+	}
+	@DeleteMapping(path = "{id}")
+	public void deletePersonById(@PathVariable("id") UUID id) {
+		personService.deletePerson(id);
+	}
+	@PutMapping(path = "{id}")
+	public void updatePersonById(@PathVariable("id") UUID id,
+		@Valid @NonNull	@RequestBody Person personToUpdate) {
+		personService.updatePerson(id, personToUpdate);
 	}
 }
